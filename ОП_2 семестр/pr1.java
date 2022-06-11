e5536a5f4fe3c1ba37f36c0fe9d7dfccb3bfa47f
m4sterb4stion <90902569+m4sterb4stion@users.noreply.github.com>
2022-06-11 06:17:34 +0300

    Delete pr1.java

--- ОП_2 семестр/pr1.java
+++
@@ -1,8 +0,0 @@
-public class pr1 {
-	 public static void main(String[] imya) {
-	        if (imya.length>0)
-	            System.out.println("Hello "+imya[0]);
-	        else
-	            System.out.println("Hello world");
-	    }
-}
