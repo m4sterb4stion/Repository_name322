086f07564410247df9b8bc465028340bc470e2b4
m4sterb4stion <90902569+m4sterb4stion@users.noreply.github.com>
2022-06-11 07:07:35 +0300

    Update pr1_2.java

--- pr1/pr1_2.java
+++ pr1/pr1_2.java
@@ -1,13 +1,12 @@
-package op;
 /* Написать программу получающую на вход в качестве аргумента несколько параметров. В программе вывести "Вы ввели" + N
 (количество параметров) + "параметров". Если параметры не передвавались, вывести "Вы не передавали параметров". */
 public class pr1_2 {
 	 public static void main(String[] args) {
 	        if (args.length != 0) {
-	            System.out.print("vi vveli "+ args.length +" perametrov");
+	            System.out.print("Вы ввели "+ args.length +" параметров");
 	        }
 	        else {
-	            System.out.print("vi ne peredavali parametrov");
+	            System.out.print("Вы не передавали параметров");
 	        }
 	    }
 }
