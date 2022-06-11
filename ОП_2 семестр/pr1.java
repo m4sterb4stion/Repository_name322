5ccd93d3911fac1d86f626b9db96d25eb29834a9
m4sterb4stion <90902569+m4sterb4stion@users.noreply.github.com>
2022-06-11 05:50:25 +0300

    Add files via upload

---
+++ ОП_2 семестр/pr1.java
@@ -0,0 +1,10 @@
+package pr1;
+
+public class pr1 {
+	 public static void main(String[] imya) {
+	        if (imya.length>0)
+	            System.out.println("Hello "+imya[0]);
+	        else
+	            System.out.println("Hello world");
+	    }
+}
